bca1e731c7e98f55bd711cf4e9e07db1b8c8e6bf
Deep Bag <deepbag92@gmail.com>
2025-02-01 16:19:23 +0530

    expoted tooltip in package

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,6 +2,7 @@
 export { default as ReactGridX } from "./components/ReactGridX";
 export { default as RGXTablePagination } from "./components/Paginations/RGXTablePagination";
 export { default as RGXArrowPagination } from "./components/Paginations/RGXArrowPagination";
+export { default as RGXTooltip } from "./components/Tooltip";
 
 // interface props exports
 export { default as RGXTablePaginationProps } from "./components/Paginations/RGXTablePagination";
