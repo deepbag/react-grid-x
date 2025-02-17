03634be6eb4d401add5de2308ed87cddc6ed881b
Deep Bag <deepbag92@gmail.com>
2025-02-17 22:01:54 +0530

    build

--- package/src/components/ReactGridX/index.tsx
+++ package/src/components/ReactGridX/index.tsx
@@ -726,11 +726,11 @@ const ReactGridX: React.FC<ReactGridXProps> = ({
                             ); // Toggle expanded row
                           }}
                         >
-                          <FontAwesomeIcon
-                            icon={
+                           <SvgIcon
+                            svgPath={
                               expandedRow === rowIndex
-                                ? solidIcons.faChevronDown
-                                : solidIcons.faChevronRight
+                                ? `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>`
+                                : `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6"/></svg>`
                             }
                             className="rgx-table-expanded-arrow-icon"
                             style={{
